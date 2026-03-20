551f3705947b5147482e2f8a1172efa1c113b78d
ShauLeo <ausbildungfi.2022@gmail.com>
2026-03-20 14:40:13 +0100

    Fix migration dependencies for NetBox v4.5.4

--- netbox_credentials/migrations/0001_initial.py
+++ netbox_credentials/migrations/0001_initial.py
@@ -9,65 +9,23 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ("dcim", "0001_initial"),
-        ("extras", "0001_initial"),
+        ("dcim", "0225_gfk_indexes"),
     ]
 
     operations = [
         migrations.CreateModel(
             name="Credential",
             fields=[
-                (
-                    "id",
-                    models.BigAutoField(
-                        auto_created=True,
-                        primary_key=True,
-                        serialize=False,
-                    ),
-                ),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False)),
                 ("created", models.DateTimeField(auto_now_add=True, null=True)),
                 ("last_updated", models.DateTimeField(auto_now=True, null=True)),
-                (
-                    "custom_field_data",
-                    models.JSONField(
-                        blank=True,
-                        default=dict,
-                        encoder=utilities.json.CustomFieldJSONEncoder,
-                    ),
-                ),
-                (
-                    "name",
-                    models.CharField(
-                        max_length=200,
-                        unique=True,
-                        help_text="Friendly name, e.g. 'default-pw' or 'snmp-ro-community'",
-                    ),
-                ),
-                (
-                    "username",
-                    models.CharField(max_length=200, blank=True, default=""),
-                ),
-                (
-                    "password",
-                    models.TextField(
-                        blank=True,
-                        default="",
-                        db_column="password",
-                        verbose_name="Password (encrypted)",
-                    ),
-                ),
-                (
-                    "description",
-                    models.CharField(max_length=500, blank=True, default=""),
-                ),
+                ("custom_field_data", models.JSONField(blank=True, default=dict, encoder=utilities.json.CustomFieldJSONEncoder)),
+                ("name", models.CharField(max_length=200, unique=True, help_text="Friendly name, e.g. 'default-pw' or 'snmp-ro-community'")),
+                ("username", models.CharField(max_length=200, blank=True, default="")),
+                ("password", models.TextField(blank=True, default="", db_column="password", verbose_name="Password (encrypted)")),
+                ("description", models.CharField(max_length=500, blank=True, default="")),
                 ("comments", models.TextField(blank=True, default="")),
-                (
-                    "tags",
-                    taggit.managers.TaggableManager(
-                        through="extras.TaggedItem",
-                        blank=True,
-                    ),
-                ),
+                ("tags", taggit.managers.TaggableManager(through="taggit.TaggedItem", blank=True)),
             ],
             options={
                 "ordering": ["name"],
@@ -78,56 +36,14 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name="DeviceCredential",
             fields=[
-                (
-                    "id",
-                    models.BigAutoField(
-                        auto_created=True,
-                        primary_key=True,
-                        serialize=False,
-                    ),
-                ),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False)),
                 ("created", models.DateTimeField(auto_now_add=True, null=True)),
                 ("last_updated", models.DateTimeField(auto_now=True, null=True)),
-                (
-                    "custom_field_data",
-                    models.JSONField(
-                        blank=True,
-                        default=dict,
-                        encoder=utilities.json.CustomFieldJSONEncoder,
-                    ),
-                ),
-                (
-                    "role",
-                    models.CharField(
-                        max_length=100,
-                        blank=True,
-                        default="",
-                        help_text="Optional role, e.g. 'management', 'snmp', 'api'",
-                    ),
-                ),
-                (
-                    "credential",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        related_name="assignments",
-                        to="netbox_credentials.credential",
-                    ),
-                ),
-                (
-                    "device",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        related_name="credential_assignments",
-                        to="dcim.device",
-                    ),
-                ),
-                (
-                    "tags",
-                    taggit.managers.TaggableManager(
-                        through="extras.TaggedItem",
-                        blank=True,
-                    ),
-                ),
+                ("custom_field_data", models.JSONField(blank=True, default=dict, encoder=utilities.json.CustomFieldJSONEncoder)),
+                ("role", models.CharField(max_length=100, blank=True, default="", help_text="Optional role, e.g. 'management', 'snmp', 'api'")),
+                ("credential", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name="assignments", to="netbox_credentials.credential")),
+                ("device", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name="credential_assignments", to="dcim.device")),
+                ("tags", taggit.managers.TaggableManager(through="taggit.TaggedItem", blank=True)),
             ],
             options={
                 "ordering": ["credential__name"],
